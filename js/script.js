77816594c31128b330d224ad2ae88a78367c59f8
SterrenHeer <sterrenheer@gmail.com>
2024-06-12 14:05:55 +0300

    add media for header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 $.get('header.html',function(response){ 
     $('.header').html(response); 
+    $('.burger_button, .burger_close').click(() => {
+        $('.burger').toggleClass( "show" )
+    });
 });
