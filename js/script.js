4207b0d2401dc3b884d255ef6481f17f0cfdd4be
SterrenHeer <sterrenheer@gmail.com>
2024-06-12 14:35:47 +0300

    add fixed position for header

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ $.get('header.html',function(response){
         $('.burger').toggleClass( "show" )
     });
 });
+
+let prevPos = window.pageYOffset;
+window.onscroll = function() {
+    var curPos = window.pageYOffset;
+    if (prevPos > curPos) {
+        document.querySelector(".header").style.top = "0";
+    } else {
+        document.querySelector(".header").style.top = "-86px";
+        if (window.matchMedia('(max-width: 992px)').matches) {
+            document.querySelector(".header").style.top = "-65px";
+        }
+    }
+    prevPos = curPos;
+}
